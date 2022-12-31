43ea81e2d038657b6fe67ea5596b27a690e33f3f
ducanh97jp <ducanhnguyen.19941997@gmail.com>
2022-12-31 17:56:58 +0900

    add

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -6,31 +6,33 @@ import reportWebVitals from './reportWebVitals';
 // import BasicForm from './Form/BasicForm';
 // import SignupForm from './Form/SignupForm';
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import StudentList from './exercise/StudentList';
 
 // const element = <h1>Xin chao</h1>;
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(element);
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const tick = () => {
-  root.render(
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-};
+// const tick = () => {
+//   root.render(
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// };
 
-setInterval(tick, 1000);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   // <SignupForm/>
-//   // <React.StrictMode>
-//   //   <App />
-//   // </React.StrictMode>
-// );
+// setInterval(tick, 1000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <SignupForm/>
+  <StudentList/>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
